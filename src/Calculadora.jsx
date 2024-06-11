2565db5c6423d312e7cfed70df25cf52c371bea1
Cristopher Paiva <josuepaiva7@gmail.com>
2024-06-11 23:16:04 +0000

    First commit

---
+++ src/Calculadora.jsx
@@ -0,0 +1,16 @@
+export const soma = (a, b) =>{
+ return a + b;
+}
+
+export const resta = (a, b) =>{
+    return a - b;
+}
+
+
+export const imprime = (valor)=>{
+    return(
+        <>
+        <h2 className="amarilla">{valor}</h2>
+        </>
+    );
+}
